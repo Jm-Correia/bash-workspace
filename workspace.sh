d6074b77541d1357f443e5945b57682e7e78969c
joao.correia <joao.correia.marcos@gmail.com>
2023-06-18 21:03:38 -0300

    add: add flatpak commands

--- workspace.sh
+++ workspace.sh
@@ -17,9 +17,19 @@ sudo apt install timeshift;
 
 echo "Download and install NVM";
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-source ~/ .bashrc
+export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source ~/.bashrc
 
-echo "looking for application.list";
+echo "Download and install RUST LANGUAGE"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.cargo/bin
+
+rust=$(rustc --version)
+echo $rust
+
+echo "looking for input.txt";
 echo "List  all inside the actual directory";
 ls ./list
 echo " ";
@@ -29,5 +39,18 @@ path="./list/input.txt"
 while read -r p; do 
     echo $p;
     file=$(eval "$p");
-    echo $file >> "output.log";
+    echo $file >> "./output/apt-get.log";
 done < $path
+
+echo "Looking for flatpak.in";
+echo "List  all inside the actual directory";
+ls ./flat
+echo " ";
+flatpakPath="./flat/flatpak.in"
+
+# O Arquivo final precisa ter um \n
+while read -r f; do 
+    echo $f;
+    file=$(eval "$f");
+    echo $file >> "./output/flatpak.log";
+done < $flatpakPath
